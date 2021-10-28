f01e6633e3d6661479ff8d4d4ba35de3267e49e3
lucasrdalcol <lucasrdalcol@ua.pt>
2021-10-28 18:22:54 +0100

    Initial commit and scripts to work on.

---
+++ Typing_test/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+# --------------------------------------------------
+# IMPORT MODULES
+# --------------------------------------------------
+from my_classes import *
+from my_functions import *
+
+
+# --------------------------------------------------
+# Script of a typing test - Assignment 1. Examples of similar softwares here: https://www.typingtest.com/
+#                                                                             https://www.youtube.com/watch?v=6tRTOd5vPH8
+#
+# Contributors:
+#   - Lucas Rodrigues Dal'Col
+#   - Vinícius Campos Batista
+#   - José Pedro Moura Costa Pinto
+#   - Rodrigo Dinis Martins Ferreira
+#
+# PSR, University of Aveiro, November 2021.
+# --------------------------------------------------
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
