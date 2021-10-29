484269256ceb6edda7a24aa193521e339d507063
lucasrdalcol <lucasrdalcol@ua.pt>
2021-10-29 17:44:00 +0100

    Almost done. check point.

--- Typing_test/main.py
+++ Typing_test/main.py
@@ -3,8 +3,14 @@
 # --------------------------------------------------
 # IMPORT MODULES
 # --------------------------------------------------
+import argparse
+from collections import namedtuple
+from time import time, ctime
+from colorama import Fore, Back, Style
+from numpy.random import randint
 from my_classes import *
 from my_functions import *
+import readchar
 
 
 # --------------------------------------------------
@@ -20,8 +26,80 @@ from my_functions import *
 # PSR, University of Aveiro, November 2021.
 # --------------------------------------------------
 
+# ------------------------
+##   DATA STRUCTURES   ##
+# ------------------------
+Input = namedtuple('Input', ['requested', 'received', 'duration'])
+
 def main():
-    pass
+    # ---------------------------------------------------
+    # Initialization
+    # ---------------------------------------------------
+    # Use argparse to give arguments to the script in terminal.
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-utm', '--use_time_mode', action='store_true',
+                    help='Max number of seconds ' + Fore.RED + 'for time' + Fore.RESET +
+                         ' mode or maximum number of inputs ' + Fore.RED + 'for' + Fore.RESET + ' number of inputs mode')
+    ap.add_argument('-mv', '--max_value', type=int, required=True,
+                    help='Max number of seconds ' + Fore.RED + 'for time' + Fore.RESET +
+                         ' mode or maximum number of inputs ' + Fore.RED + 'for' + Fore.RESET + ' number of inputs mode')
+    args = vars(ap.parse_args())
+
+    # Ask to press any key to start.
+    if args['use_time_mode']:
+        print('You are in time mode and you have ' + Fore.RED + str(args['max_value']) +
+              Fore.RESET + ' seconds to complete the test.')
+    else:
+        print('You are in maximum number of inputs mode and you have ' + Fore.RED + str(args['max_value']) +
+              Fore.RESET + ' inputs available to complete the test.')
+    print("Press any key to start the the test.")
+    readchar.readkey()
+
+    # Initialize parameters dict
+    parameters = {}
+
+    # Counter for the number of inputs
+    number_inputs = 0
+    number_of_hits = 0
+
+
+    # While cycle for each time mode
+    start_time = time()
+    parameters['test_start'] = ctime()
+
+    while (number_inputs < args['max_value'] and args['use_time_mode'] is False) \
+            or (args['use_time_mode'] is True and time() - start_time < args['max_value']):
+
+        # Generate random low case letter
+        low_case = randint(97, 122)
+        print('Type letter ' + Fore.BLUE + chr(low_case) + Fore.RESET)
+
+
+        # Get pressed key from keyboard
+        pressed_key = readchar.readkey()
+
+        # Analyse pressed key to see if it's a space, to stop the code.
+        if pressed_key == str(' '):
+            break
+
+        # Check the pressed key with the random low case letter
+        if pressed_key == chr(low_case):
+            print('You typed letter ' + Fore.GREEN + pressed_key + Fore.RESET)
+            number_of_hits += 1
+        else:
+            print('You typed letter ' + Fore.RED + pressed_key + Fore.RESET)
+
+        number_inputs += 1
+
+    print('The test is over. See you next time!')
+
+    parameters['test_end'] = ctime()
+    parameters['number_of_types'] = number_inputs
+    parameters['number_of_hits'] = number_of_hits
+    parameters['number_of_misses'] = number_inputs - number_of_hits
+    parameters['accuracy'] = number_of_hits/number_inputs
+
+    print(parameters)
 
 
 if __name__ == "__main__":
--- Typing_test/my_functions.py
+++ Typing_test/my_functions.py
@@ -3,6 +3,7 @@
 # --------------------------------------------------
 # IMPORT MODULES
 # --------------------------------------------------
+from time import time
 
 # ------------------------
 ##   DATA STRUCTURES   ##
@@ -12,4 +13,29 @@
 # ------------------------
 ## FUNCTION DEFINITION ##
 # ------------------------
+def tic():
+    """
+    Functions used to return the numbers of seconds passed since epoch to use with function toc() afterwards.
+    Like tic toc matlab functions.
+    :return: a float.
+    """
 
+    global start_time
+    start_time = time()
+
+
+def toc():
+    """
+    Function used to print the elapsed time since function tic() was used. tic() and toc() works together.
+    :return: a float.
+    """
+
+    # Get the number os seconds passed since epoch and subtract from tic(). This is the elapsed time from tic to toc.
+
+    # Check if start_time is inside global variables
+    if 'start_time' in globals():
+        last_time = time()
+        elapsed_time = last_time - start_time
+        print('Elapsed time: ' + str(elapsed_time) + ' seconds.')
+    else:
+        print('Error: start time from tic() not set')
