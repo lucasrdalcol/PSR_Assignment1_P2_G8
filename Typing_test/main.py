201e05504131a7bbf5fd4c8c9013f678b4efd1b4
lucasrdalcol <lucasrdalcol@ua.pt>
2021-10-31 22:54:23 +0000

    All done, including issue #6. Maybe we could do more improvements.

--- Typing_test/main.py
+++ Typing_test/main.py
@@ -5,16 +5,19 @@
 # --------------------------------------------------
 import argparse
 from collections import namedtuple
+from pprint import pprint
 from prettyprinter import cpprint
 from time import time, ctime
 from colorama import Fore, Back, Style
 from numpy.random import randint
 from my_classes import *
 from my_functions import *
+from my_functions import Input
 import readchar
 from statistics import mean
 from prettyprinter import set_default_style
-
+from prettyprinter import install_extras
+install_extras(['python'])
 set_default_style('light')
 
 # --------------------------------------------------
@@ -29,11 +32,6 @@ set_default_style('light')
 #
 # PSR, University of Aveiro, November 2021.
 # --------------------------------------------------
-
-# ------------------------
-##   DATA STRUCTURES   ##
-# ------------------------
-Input = namedtuple('Input', ['requested', 'received', 'duration'])
 
 
 def main():
@@ -51,9 +49,9 @@ def main():
     args = vars(ap.parse_args())
 
     # Print the description of the typing test.
-    print('Welcome to our Typing Test. \n\nContributors: \n- Lucas Rodrigues DalCol \n- Vinícius Campos de Oliveira '
-          'Batista \n- José Pedro Moura Costa Pinto \n- Rodrigo Dinis Martins Ferreira \n\n PSR, University of Aveiro, '
-          'November 2021.\n')
+    print("Welcome to our Typing Test. \n\nContributors: \n- Lucas Rodrigues Dal'Col \n- Vinícius Campos de Oliveira "
+          "Batista \n- José Pedro Moura Costa Pinto \n- Rodrigo Dinis Martins Ferreira \n\n PSR, University of Aveiro, "
+          "November 2021.\n")
 
     # See which mode is being used and ask to press any key to start.
     if args['use_time_mode']:
@@ -63,63 +61,23 @@ def main():
         print('You are in maximum number of inputs mode and you have ' + Fore.RED + str(args['max_value']) +
               Fore.RESET + ' inputs available to complete the test.')
     print("\nPress any key to start the the test.\n")
+
     readchar.readkey()  # Block the code waiting for a key to start
     start_time = tic()  # Start the counter of the test
 
-    # Initialize parameters dict
-    parameters = {}
+    # Initialize parameters dict and start date of the test and put in the dict
+    parameters = {'test_start': ctime()}
 
-    # Initialize list of inputs and durations averages
-    input_list = []
-    # idx_hit = []
-    type_hit_average_duration = []
-    type_miss_average_duration = []
-
-    # Counter for the number of inputs
-    number_inputs = 0
-    number_of_hits = 0
-
-    parameters['test_start'] = ctime()  # Start date of the test and put in the dict
-
-    # While cycle for each time mode
-    while (args['use_time_mode'] is False and number_inputs < args['max_value']) \
-            or (args['use_time_mode'] is True and time() - start_time < args['max_value']):
-
-        # Generate random low case letter
-        low_case = randint(97, 122)
-        print('Type letter ' + Fore.BLUE + chr(low_case) + Fore.RESET)
-
-        # Get pressed key from keyboard and time of response
-        type_time_start = tic()
-        pressed_key = readchar.readkey()
-        type_elapsed_time = toc(type_time_start)
-
-        # Analyse pressed key to see if it's a space, to stop the code.
-        if pressed_key == str(' '):
-            print('\nYou pressed the space bar, test aborted.')
-            exit(0)
-
-        # Create tuple with requested and received key with its durations and append to a list for the dict later on.
-        input_tuple = Input(requested=chr(low_case), received=pressed_key, duration=type_elapsed_time)
-        input_list.append(input_tuple)
-
-        # Check the pressed key with the random low case letter
-        if pressed_key == chr(low_case):
-            print('You typed letter ' + Fore.GREEN + pressed_key + Fore.RESET)
-            number_of_hits += 1
-            # idx_hit.append(number_inputs)
-            type_hit_average_duration.append(type_elapsed_time)
-        else:
-            print('You typed letter ' + Fore.RED + pressed_key + Fore.RESET)
-            type_miss_average_duration.append(type_elapsed_time)
-
-        number_inputs += 1
+    # Call typing test function for each time mode
+    if args['use_time_mode']:
+        input_list, number_inputs, number_of_hits, type_average_durations, type_hit_average_duration, \
+        type_miss_average_duration = typing_test(use_time_mode=True, max_value=args['max_value'])
+    else:
+        input_list, number_inputs, number_of_hits, type_average_durations, type_hit_average_duration, \
+        type_miss_average_duration = typing_test(use_time_mode=False, max_value=args['max_value'])
 
     test_elapsed_time = toc(start_time)
     print('\nThe test is over. Here it is your statistics: \n')
-
-    type_average_durations = [type_duration.duration for type_duration in input_list]
-    # type_hit_average_duration = [type_average_duration for idx, type_average_duration in enumerate(type_average_durations)]
 
     # Add more dictionary keys with the other parameters requested.
     parameters['test_end'] = ctime()
@@ -140,7 +98,9 @@ def main():
         parameters['type_miss_average_duration'] = 0.0
 
     # Pretty print with colors
-    cpprint(parameters, sort_dict_keys=True)
+    # cpprint(parameters, sort_dict_keys=True)
+    # pprint(parameters)
+    pprint_color(parameters)
 
     print('\nThank you. See you next time.')
 
--- Typing_test/my_functions.py
+++ Typing_test/my_functions.py
@@ -3,12 +3,22 @@
 # --------------------------------------------------
 # IMPORT MODULES
 # --------------------------------------------------
-from time import time
+from collections import namedtuple
+from colorama import Fore
+from numpy.random import randint
+import time
+from my_classes import *
+import readchar
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import Terminal256Formatter
+from pprint import pformat
 
 
 # ------------------------
 ##   DATA STRUCTURES   ##
 # ------------------------
+Input = namedtuple('Input', ['requested', 'received', 'duration'])
 
 
 # ------------------------
@@ -22,7 +32,7 @@ def tic():
     """
 
     # Get the number os seconds passed since epoch
-    start_time = time()
+    start_time = time.time()
 
     return start_time
 
@@ -35,7 +45,96 @@ def toc(start_time):
     """
 
     # Get the number of seconds passed since epoch and subtract from tic(). This is the elapsed time from tic to toc.
-    end_time = time()
+    end_time = time.time()
     elapsed_time = end_time - start_time
 
     return elapsed_time
+
+
+def typing_test(use_time_mode, max_value):
+
+    # Initialize list of inputs and durations averages
+    input_list = []
+    type_hit_average_duration = []
+    type_miss_average_duration = []
+
+    # Counter for the number of inputs
+    number_inputs = 0
+    number_of_hits = 0
+
+    if use_time_mode:
+        try:  # Stop the program immediately when time runs out
+            with Timeout(max_value):  # Call class Timeout with the max value
+                while True:
+                    # Generate random low case letter
+                    low_case = randint(97, 122)
+                    print('Type letter ' + Fore.BLUE + chr(low_case) + Fore.RESET)
+
+                    # Get pressed key from keyboard and time of response
+                    type_time_start = tic()
+                    pressed_key = readchar.readkey()
+                    type_elapsed_time = toc(type_time_start)
+
+                    # Analyse pressed key to see if it's a space, to stop the code.
+                    if pressed_key == str(' '):
+                        print('\nYou pressed the space bar, test aborted.')
+                        exit(0)
+
+                    # Create tuple with requested and received key with its durations and append to a list for the
+                    # dict later on.
+                    input_tuple = Input(requested=chr(low_case), received=pressed_key, duration=type_elapsed_time)
+                    input_list.append(input_tuple)
+
+                    # Check the pressed key with the random low case letter
+                    if pressed_key == chr(low_case):
+                        print('You typed letter ' + Fore.GREEN + pressed_key + Fore.RESET)
+                        number_of_hits += 1
+                        # idx_hit.append(number_inputs)
+                        type_hit_average_duration.append(type_elapsed_time)
+                    else:
+                        print('You typed letter ' + Fore.RED + pressed_key + Fore.RESET)
+                        type_miss_average_duration.append(type_elapsed_time)
+
+                    number_inputs += 1
+
+        except Timeout.Timeout:
+            pass
+
+    else:
+        while number_inputs < max_value:
+            # Generate random low case letter
+            low_case = randint(97, 122)
+            print('Type letter ' + Fore.BLUE + chr(low_case) + Fore.RESET)
+
+            # Get pressed key from keyboard and time of response
+            type_time_start = tic()
+            pressed_key = readchar.readkey()
+            type_elapsed_time = toc(type_time_start)
+
+            # Analyse pressed key to see if it's a space, to stop the code.
+            if pressed_key == str(' '):
+                print('\nYou pressed the space bar, test aborted.')
+                exit(0)
+
+            # Create tuple with requested and received key with its durations and append to a list for the dict later on.
+            input_tuple = Input(requested=chr(low_case), received=pressed_key, duration=type_elapsed_time)
+            input_list.append(input_tuple)
+
+            # Check the pressed key with the random low case letter
+            if pressed_key == chr(low_case):
+                print('You typed letter ' + Fore.GREEN + pressed_key + Fore.RESET)
+                number_of_hits += 1
+                type_hit_average_duration.append(type_elapsed_time)
+            else:
+                print('You typed letter ' + Fore.RED + pressed_key + Fore.RESET)
+                type_miss_average_duration.append(type_elapsed_time)
+
+            number_inputs += 1
+
+    type_average_durations = [type_duration.duration for type_duration in input_list]
+
+    return input_list, number_inputs, number_of_hits, type_average_durations, type_hit_average_duration, \
+           type_miss_average_duration
+
+def pprint_color(obj):
+    print(highlight(pformat(obj), PythonLexer(), Terminal256Formatter()))
